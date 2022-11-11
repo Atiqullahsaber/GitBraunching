38e761606f2e6d810b00fc05cd7d0f1e983e856d
panthersClass <atiqullah_esmat@yahoo.com>
2022-11-11 09:45:30 -0800

    panthers branch

--- src/test/Braunching.java
+++ src/test/Braunching.java
@@ -3,7 +3,7 @@ package test;
 public class Braunching {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this is inside the panthers branch");
 
 	}
 
